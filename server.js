739eeca7f48bc92c976483b7805c1d9e6d9ebed1
Prince kumar <prince.m2hgamerz@gmail.com>
2025-11-28 15:10:31 +0530

    path fix v3

--- server.js
+++ server.js
@@ -222,17 +222,17 @@ async function storeVideoData(tweetId, result) {
   }
 }
 
-// Routes - SIMPLIFIED FOR VERCEL
+// Routes - Using redirects instead
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/playground', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'playground.html'));
+  res.redirect('/playground.html');
 });
 
 app.get('/docs', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'docs.html'));
+  res.redirect('/docs.html');
 });
 
 // Main download endpoint
